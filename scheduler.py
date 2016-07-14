dc08102bd76d4116cd8469cd9cb81534a5a5f0f4
zgong <gongzhenggz@gmail.com>
2016-07-14 00:02:18 -0400

    add more comments

--- scheduler.py
+++ scheduler.py
@@ -1,4 +1,9 @@
 '''
+This is a scheduler to pick and remind people of Scrum of Scrums. 
+It will send out an email and a SMS(you can opt off this) to the person on duty.
+
+You can configure the number of weeks in rotation and add new users to it, it's complete fair game!
+
 Created on 2016-07-13
 @author: Zheng Gong
 '''
@@ -7,6 +12,14 @@ from send_sms import SendSMS
 from send_email import SendEmail
 
 def run(sendSMS, message, num_of_weeks=3):
+    '''
+    Make each receiver a config file, which contains only a number, indicating
+    current count of weeks. 
+    Assume we have 3-week rotation:
+    1. when all are 3 => all done => reset every count to 0 to restart
+    2. when all are 0(or some are 3 else are 0) => select a random name from names with 0 count and increase count by 1
+    3. where there is a non-zero count => continue using that guy and increase count by 1
+    '''
     receiver_dict = sendSMS.receiverDict
     config = {}
     for receiver in receiver_dict.keys():
@@ -44,18 +57,18 @@ def run(sendSMS, message, num_of_weeks=3):
     if target_person:
         print target_person
         message = message % target_person
-        # do not send sms (they need to pay for receiving sms)
+        sendEmail.send_email_to_many([target_person], message)
+        # do not send sms to certain people (they need to pay for receiving sms)
         if target_person != 'Glenn':
             sendSMS.send_sms_to_many([target_person], message)
-        sendEmail.send_email_to_many([target_person], message)
-
+        
 
 if __name__=='__main__':
     #,'Xinkai':'3479331959'
     # {'Glenn':'6174298743'}
     # 'Yuge':'6174355509'
     # {'Zheng':('4123541459','gongzhenggz@gmail.com')}
-    receiver_dict = {'Yuge':('6174355509','yugexiao2014@gmail.com')}
+    receiver_dict = {'Zheng':('4123541459','gongzhenggz@gmail.com')}
     message = "Hey %s: it's your turn to attend Scrum of Scrum today!"
     sendSMS = SendSMS(receiver_dict)
     sendEmail = SendEmail(receiver_dict)
